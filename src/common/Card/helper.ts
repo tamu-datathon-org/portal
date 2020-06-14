ef771288765178cba0c49c39234df7182381b479
Rishabh <rtatia65@gmail.com>
2020-06-13 22:30:26 -0500

    implemented moment.js for formatting time

--- src/common/Card/helper.ts
+++ src/common/Card/helper.ts
@@ -1,18 +1,4 @@
-// just a list of all months in order
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
+import moment from "moment";
 
 /**
  * Formats date to a string for the start and end time.
@@ -24,23 +10,10 @@ export const formatTime = (time: Date, duration: number): string => {
    * returns string in HH:MM(AM/PM) format
    * @param time a date object
    */
-  const stringify = (time: Date): string => {
-    const hours: number = time.getHours();
-    const meridiem: string = hours > 11 ? "PM" : "AM";
-    let minutes: string = time.getMinutes().toString();
-    if (minutes.length < 2) minutes = "0" + minutes;
-    const hourStr: string = hours % 12 == 0 ? "12" : (hours % 12).toString();
-
-    return hourStr + ":" + minutes + meridiem;
-  };
 
   return (
-    months[time.getMonth()] +
-    " " +
-    time.getDate() +
-    " " +
-    stringify(time) +
+    moment(time).format("MMMM Do h:mm a") +
     " to " +
-    stringify(new Date(time.getTime() + duration * 60000))
+    moment(new Date(time.getTime() + duration * 60000)).format("h:mm a")
   );
 };
