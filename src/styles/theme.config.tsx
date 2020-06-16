cda0dabd237b925a2126b064e23aa45a32c82413
Samarth Dave <samarthdave@users.noreply.github.com>
2020-06-16 14:39:40 -0500

    Resolve linting errors

--- src/styles/theme.config.tsx
+++ src/styles/theme.config.tsx
@@ -8,10 +8,10 @@ export class THEMES {
       primary: "#0070f3",
     },
     // TAMU Datathon Theme Colors
-    theme_dark_green: '#22b9a5',
-    theme_light_green: '#86eace',
-    theme_light_blue: '#4fc2f8',
-    splash_light_purple: '#7c408a',
-    splash_light_blue: '#3f5aaa',
+    theme_dark_green: "#22b9a5",
+    theme_light_green: "#86eace",
+    theme_light_blue: "#4fc2f8",
+    splash_light_purple: "#7c408a",
+    splash_light_blue: "#3f5aaa",
   };
 }
