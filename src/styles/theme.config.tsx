1d000702132f0c16fe88b4b378b6b2027c5621e4
Samarth Dave <samarth@MacBook-Pro.local>
2020-06-20 15:31:13 -0500

    Add remaining color variables from sass

--- src/styles/theme.config.tsx
+++ src/styles/theme.config.tsx
@@ -5,13 +5,44 @@ export class THEMES {
   static readonly PrimaryTheme = {
     // original value from _app.tsx
     colors: {
-      primary: "#0070f3",
+      white: "#fff",
+      gray100: "#f8f9fa",
+      gray200: "#e9ecef",
+      gray300: "#dee2e6",
+      gray400: "#ced4da",
+      gray500: "#adb5bd",
+      gray600: "#6c757d",
+      gray700: "#495057",
+      gray800: "#343a40",
+      gray900: "#212529",
+      black: "#000",
+
+      blue: "#4286f3",
+      indigo: "#4740af",
+      purple: "#6f42c1",
+      pink: "#e462a3",
+      red: "#dc3545",
+      orange: "#fd7e14",
+      yellow: "#ffc107",
+      green: "#28a745",
+      teal: "#86eace",
+      cyan: "#17a2b8",
+
+      primary: "#4740af",
+      secondary: "#6c757d",
+      success: "#28a745",
+      info: "#4286f3",
+      warning: "#ffc107",
+      danger: "#dc3545",
+      light: "#f8f9fa",
+      dark: "#343a40",
+
+      // TAMU Datathon Theme Colors
+      theme_dark_green: "#22b9a5",
+      theme_light_green: "#86eace",
+      theme_light_blue: "#4fc2f8",
+      splash_light_purple: "#7c408a",
+      splash_light_blue: "#3f5aaa",
     },
-    // TAMU Datathon Theme Colors
-    theme_dark_green: "#22b9a5",
-    theme_light_green: "#86eace",
-    theme_light_blue: "#4fc2f8",
-    splash_light_purple: "#7c408a",
-    splash_light_blue: "#3f5aaa",
   };
 }
