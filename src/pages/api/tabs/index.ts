a0afe09f22e8717d84fe6fd1778864a80bcd91f2
Daniel Abreo <dabreo43@gmail.com>
2020-06-23 23:41:59 -0500

    initial commit

--- src/pages/api/tabs/index.ts
+++ src/pages/api/tabs/index.ts
@@ -7,16 +7,16 @@ export default async function handler<JSON = unknown>(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const files = await fs.promises.readdir("db/pages/");
-  const arr: Array<string> = [];
-  for (const file of files) {
-    if (path.extname(file).toLowerCase() === ".yaml") {
-      const fileContent = await fs.promises.readFile(
-        "db/pages/" + file,
-        "utf8"
-      );
-      arr.push(yaml.safeLoad(fileContent));
-    }
-  }
-  return res.status(200).json(arr);
+  // const files = await fs.promises.readdir("db/pages/");
+  // const arr: Array<string> = [];
+  // for (const file of files) {
+  //   if (path.extname(file).toLowerCase() === ".yaml") {
+  //     const fileContent = await fs.promises.readFile(
+  //       "db/pages/" + file,
+  //       "utf8"
+  //     );
+  //     arr.push(yaml.safeLoad(fileContent));
+  //   }
+  // }
+  return res.status(200).json("test");
 }
