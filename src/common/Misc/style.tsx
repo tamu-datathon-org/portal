62550d4beeb4cfb02d8b7dde5c687d0407c0a5c7
Samarth Dave <samarthdave@users.noreply.github.com>
2020-06-23 22:49:41 -0500

    Switch to light grey, use variables & background

--- src/common/Misc/style.tsx
+++ src/common/Misc/style.tsx
@@ -10,21 +10,22 @@ export const CustomHeader = styled.h1`
   color: ${(props) => props.theme.colors.primary};
 `;
 
-export const EventsBlueWrapper = styled.div`
-  background: ${(props) => props.theme.colors.theme_lighter_blue};
+export const ColorSpan = styled.span`
+  color: ${(props) => props.theme.colors.indigo};
+  font-weight: bold;
+  box-shadow: 0 -0.8em 0 0 ${(props) => props.theme.colors.gray100} inset;
+  text-decoration: underline;
 `;
 
-export const ColorSpan = styled.span`
-  box-shadow: 0 -0.5em 0 0 ${(props) => props.theme.colors.theme_lighter_blue} inset;
-  text-decoration: underline;
+export const EventsBlueWrapper = styled.div`
+  background: ${(props) => props.theme.colors.gray100};
 `;
 
 export const NavItem = styled(Nav.Item)`
   margin: 0 0.5rem;
   .active {
-    color: #4740af !important;
-    background-color: ${(props) =>
-      props.theme.colors.theme_lighter_blue}!important;
+    color: ${(props) => props.theme.colors.indigo} !important;
+    background-color: ${(props) => props.theme.colors.gray100} !important;
   }
 `;
 
