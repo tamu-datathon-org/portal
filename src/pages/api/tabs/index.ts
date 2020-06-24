e00840f8b957d2739cdc6c6d32b84c19acc4cf41
Daniel Abreo <dabreo43@gmail.com>
2020-06-23 23:57:55 -0500

    step 4

--- src/pages/api/tabs/index.ts
+++ src/pages/api/tabs/index.ts
@@ -1,13 +1,14 @@
 import yaml from "js-yaml";
 import fs from "graceful-fs";
 import * as path from "path";
+import { join } from "path";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler<JSON = unknown>(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const files = await fs.promises.readdir("/");
+  const files = await fs.promises.readdir(join(process.cwd(), "/"));
   // const arr: Array<string> = [];
   // for (const file of files) {
   //   if (path.extname(file).toLowerCase() === ".yaml") {
