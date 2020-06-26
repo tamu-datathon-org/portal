1c2acb4b3f7dce55eaf4e432f6743dca0d3d1fc1
Daniel Abreo <dabreo43@gmail.com>
2020-06-25 23:20:55 -0500

    test

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,45 @@
+import { getPostBySlug, getAllPosts } from '../../src/libs/api'
+import markdownToHtml from '../../src/libs/markdownToHtml'
+
+const Post = (post: JSON) => {
+  return (
+    JSON.stringify(post)
+  )
+}
+
+export default Post
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug)
+  const content = await markdownToHtml(post.content || '')
+  
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(['slug'])
+
+  return {
+    paths: posts.map((posts) => {
+      return {
+        params: {
+          slug: posts.slug,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
---
+++ src/libs/api.ts
@@ -0,0 +1,37 @@
+import fs from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
+
+const postsDirectory = join(process.cwd(), '_posts')
+
+export function getPostSlugs() {
+  return fs.readdirSync(postsDirectory)
+}
+
+export function getPostBySlug(slug: string) {
+  const realSlug = slug.replace(/\.md$/, '')
+  const fullPath = join(postsDirectory, `${realSlug}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const { data, content } = matter(fileContents)
+  console.log(data)
+  type Items = {
+    [key: string]: string
+  }
+
+  const items: Items = {}
+
+  Object.entries(data).forEach(([key, value]) => {
+    items[key] = data[key]
+  })
+  items['slug'] = realSlug
+  items['content'] = content
+
+  return items
+}
+
+export function getAllPosts(fields: string[] = []) {
+  const slugs = getPostSlugs()
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug))
+  return posts
+}
