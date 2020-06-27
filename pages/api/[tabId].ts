d09c73256b40a9ee916bbe772f75b1c7c712a07a
Daniel Abreo <dabreo43@gmail.com>
2020-06-26 23:18:39 -0500

    test

--- pages/api/[tabId].ts
+++ pages/api/[tabId].ts
@@ -6,11 +6,11 @@ export default async function handler<JSON = unknown>(
   { query: { tabId } }: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const file = "db/pages/" + tabId + ".yaml";
-  let str = "[]";
-  if (fs.existsSync(file)) {
-    const fileContent = await fs.promises.readFile(file, "utf8");
-    str = yaml.safeLoad(fileContent).sets;
-  }
-  return res.status(200).json(str);
+  //const file = "db/pages/" + tabId + ".yaml";
+  // let str = "[]";
+  // if (fs.existsSync(file)) {
+  //   const fileContent = await fs.promises.readFile(file, "utf8");
+  //   str = yaml.safeLoad(fileContent).sets;
+  // }
+  return res.status(200).json("hi");
 }
