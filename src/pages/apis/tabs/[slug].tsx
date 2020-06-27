e0b47e836b9dd1c0413d0afabe5952c0ef3c85ef
Daniel Abreo <dabreo43@gmail.com>
2020-06-26 23:45:56 -0500

    add tab api

---
+++ src/pages/apis/tabs/[slug].tsx
@@ -0,0 +1,42 @@
+import { getPostBySlug, getAllPosts } from "../../../libs/api";
+
+const Post = (post: JSON) => {
+  return JSON.stringify(post);
+};
+
+export default Post;
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug);
+  const content = await (post.content || "");
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(["slug"]);
+
+  return {
+    paths: posts.map((posts) => {
+      return {
+        params: {
+          slug: posts.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
