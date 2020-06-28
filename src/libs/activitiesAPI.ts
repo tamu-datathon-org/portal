c275b2ab9ebb2eee909264a6cfb904b1512fbaa0
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2020-06-28 17:57:05 -0500

    add activity detail api (#32)
    (#18) Add functions that retrieve information for Activities (to be used in getStaticProps)
    - Created lib functions that retrieve activities
    - Created Activity types
    Co-authored-by: George Thayamkery <george.thayamkery@gmail.com>

---
+++ src/libs/activitiesAPI.ts
@@ -0,0 +1,49 @@
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+
+const activityDirectory = join(process.cwd(), "db/activities");
+
+/**
+ * @returns {Promise<string[]>} names of files in activityDirectory path
+ */
+export async function getActivityNames(): Promise<string[]> {
+  return fs.promises.readdir(activityDirectory);
+}
+
+export interface Activity {
+  name: string;
+  id: string;
+  startTime: Date;
+  endTime: Date;
+  mediaType: "meeting_url" | "embed_url";
+  mediaLink: string;
+  thumbnail: string;
+  presenter: string;
+  priority: number;
+  relatedActivities: string[];
+  content: string;
+}
+
+/**
+ * Returns contents of an activity file in activityDirectory as JSON
+ * @param {string} page ID of the activity (filename without .md)
+ * @returns {Promise<Activity>}
+ */
+export async function getActivityByName(page: string): Promise<Activity> {
+  const realPage = page.replace(/\.md$/, "");
+  const fullPath = join(activityDirectory, `${realPage}.md`);
+  const fileContents = await fs.promises.readFile(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  return { ...data, content } as Activity;
+}
+
+/**
+ * @returns {Promise<Activity[]>} list of contents of the activitiy files in the activityDirectory path
+ */
+export async function getAllActivities(): Promise<Activity[]> {
+  const activityNames = await getActivityNames();
+  const pages = activityNames.map((page) => getActivityByName(page));
+  return Promise.all(pages);
+}
---
+++ src/pages/events/activities/[page].tsx
@@ -0,0 +1,44 @@
+import {
+  getActivityByName,
+  getAllActivities,
+} from "../../../libs/activitiesAPI";
+import { GetStaticProps, GetStaticPaths } from "next";
+import React from "react";
+import { Container } from "react-bootstrap";
+
+// TODO: Replace Component with a proper one!
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Page: React.FC = ({ page }: any) => {
+  return (
+    <Container>
+      <pre>{JSON.stringify(page, null, 4)}</pre>
+    </Container>
+  );
+};
+
+export default Page;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const activityName = (params ? params.page : "") as string;
+  const page = await getActivityByName(activityName);
+  return {
+    props: {
+      page,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pages = await getAllActivities();
+
+  return {
+    paths: pages.map((pages) => {
+      return {
+        params: {
+          page: pages.id,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
