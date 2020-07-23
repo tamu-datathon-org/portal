6da3f92e2d4c31ea59ff4d8da6368782500e0d9e
Samarth Dave <samarthdave@users.noreply.github.com>
2020-07-22 19:18:09 -0500

    Add favicon and Head component

---
+++ src/common/Head/Head.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import NextHead from "next/head";
+
+import { HeadProps } from "./interfaces";
+
+/**
+ * Head component
+ */
+export const Head: React.FC<HeadProps> = ({ children, title }) => {
+  const resTitle = (title && title.length !== 0) ? title : "Events - TAMU Datathon";
+  return (
+    <NextHead>
+      <title>{resTitle}</title>
+      <link rel="icon" type="image/png" href="/static/img/favicon.ico" />
+      {children}
+    </NextHead>
+  );
+};
+
+Head.propTypes = {
+  title: PropTypes.string,
+};
