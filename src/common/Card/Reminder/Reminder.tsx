9be592d9d6e338ff50e2cee384fe1cdf8dc12d54
Samarth Dave <samarthdave@users.noreply.github.com>
2020-08-17 13:10:24 -0500

    Change variable names

--- src/common/Card/Reminder/Reminder.tsx
+++ src/common/Card/Reminder/Reminder.tsx
@@ -5,8 +5,8 @@ import * as UI from "./style";
 import PropTypes from "prop-types";
 
 // Time constants for clarity
-const ONE_DAY_MINUTES = 1440;
-const FIFTEEN_MIN_MS = 15 * 60000;
+const TIME_UNTIL_HIDE_BADGE_MS = 1440 * 60000;
+const TIME_BEFORE_SHOW_BADGE_MS = 15 * 60000;
 /**
  * Reminder component
  */
@@ -14,7 +14,7 @@ export const Reminder: React.FC<ReminderProps> = ({ startTime, duration }) => {
   const [currentTime, setCurrentTime] = useState(new Date());
   // I think this is minutes since the START of the event
   const mins = Math.ceil((startTime.getTime() - currentTime.getTime()) / 60000);
-  const minutesSinceEnded = Math.abs(mins) - duration;
+  const millisecondsSinceEnded = (Math.abs(mins) - duration) * 60000;
 
   /* Updates the reminder every 15 seconds */
   useEffect(() => {
@@ -25,12 +25,12 @@ export const Reminder: React.FC<ReminderProps> = ({ startTime, duration }) => {
   }, []);
 
   /* Displays the reminder status if start time is less than 15 minutes away */
-  if (startTime.getTime() - currentTime.getTime() < FIFTEEN_MIN_MS) {
+  if (startTime.getTime() - currentTime.getTime() < TIME_BEFORE_SHOW_BADGE_MS) {
     let message = `Starts in ${mins.toString()} minutes`;
     if (mins < 0) {
       if (duration + mins > 0) {
         message = "Ongoing";
-      } else if (minutesSinceEnded < ONE_DAY_MINUTES) {
+      } else if (millisecondsSinceEnded < TIME_UNTIL_HIDE_BADGE_MS) {
         message = "Ended";
         return <UI.StyledExpiredReminder>{message}</UI.StyledExpiredReminder>;
       } else {
