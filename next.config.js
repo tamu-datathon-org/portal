ce31d1ffd5adcb03f4edc9eec6381e4299d68b16
George Thayamkery <george.thayamkery@gmail.com>
2021-04-06 22:57:02 -0500

    Upgrade next and routing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,28 @@
 const path = require("path"); // eslint-disable-line @typescript-eslint/no-var-requires
 
-const BASE_PATHNAME = "events";
+const defaultBasePath = "/events";
+const defaultFallbackUrl = "https://tamudatathon.com/:path*";
 
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname)],
   },
-  experimental: {
-    basePath: `/${BASE_PATHNAME}`,
+  module: true,
+  env: {
+    BASE_PATH: process.env.BASE_PATH || defaultBasePath,
   },
-  assetPrefix: BASE_PATHNAME ? `/${BASE_PATHNAME}` : "",
+  basePath: process.env.BASE_PATH || defaultBasePath,
+  async rewrites() {
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: "/:path*",
+          basePath: false,
+          destination: process.env.FALLBACK_URL || defaultFallbackUrl,
+        },
+      ],
+    };
+  },
 };
