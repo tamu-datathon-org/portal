f8cc88aae182c9414008d602ac23510d22f4c958
Dan Abreo <dabreo43@gmail.com>
2021-08-30 23:43:54 -0500

    read the docs

--- src/pages/api/json/index.ts
+++ src/pages/api/json/index.ts
@@ -1,5 +1,29 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import { getAllActivities } from "../../../libs/activitiesAPI";
+// import { getAllActivities } from "../../../libs/activitiesAPI";
+
+import fs from "fs";
+import matter from "gray-matter";
+import { ActivityInfoProps } from "../../../common/ActivityInfo";
+import { join } from "path";
+const activityDirectory = join(process.cwd(), "db/activities");
+export async function getActivityNames(): Promise<string[]> {
+  return fs.promises.readdir(activityDirectory);
+}
+export async function getActivityByName(
+  page: string
+): Promise<ActivityInfoProps> {
+  const realPage = page.replace(/\.md$/, "");
+  const fullPath = join(activityDirectory, `${realPage}.md`);
+  const fileContents = await fs.promises.readFile(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  return { ...data, content, id: realPage } as ActivityInfoProps;
+}
+export async function getAllActivities(): Promise<ActivityInfoProps[]> {
+  const activityNames = await getActivityNames();
+  const pages = activityNames.map((page) => getActivityByName(page));
+  return Promise.all(pages);
+}
 
 /**
  * Return JSON of all activities in /db/activities
