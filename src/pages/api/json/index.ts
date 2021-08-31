799379c85aba6950ff6031588ac3db9921f9a7cf
Dan Abreo <dabreo43@gmail.com>
2021-08-30 21:57:26 -0500

    add apis

---
+++ src/pages/api/json/index.ts
@@ -0,0 +1,15 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+import { getAllActivities } from "../../../libs/activitiesAPI";
+
+/**
+ * Return JSON of all activities in /db/activities
+ */
+const allPages = async (
+  req: NowRequest,
+  res: NowResponse
+): Promise<void> => {
+  const allPages = await getAllActivities();
+  res.status(200).json(allPages);
+};
+
+export default allPages;
