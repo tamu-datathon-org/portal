0b25028854e263fbb4ec634ad1e9edfe1fd2b448
Dan Abreo <dabreo43@gmail.com>
2021-08-30 22:01:50 -0500

    lint

--- src/pages/api/json/[page].ts
+++ src/pages/api/json/[page].ts
@@ -4,13 +4,10 @@ import { getActivityByName } from "../../../libs/activitiesAPI";
 /**
  * Return JSON of a specififc activity from /db/activities/{name_of_activity}
  */
-const activity = async (
-    req: NowRequest,
-    res: NowResponse
-): Promise<void> => {
-    const { page } = req.query;
-    const activity = await getActivityByName(String(page));
-    res.status(200).json(activity);
-}
+const activity = async (req: NowRequest, res: NowResponse): Promise<void> => {
+  const { page } = req.query;
+  const activity = await getActivityByName(String(page));
+  res.status(200).json(activity);
+};
 
 export default activity;
--- src/pages/api/json/index.ts
+++ src/pages/api/json/index.ts
@@ -4,10 +4,7 @@ import { getAllActivities } from "../../../libs/activitiesAPI";
 /**
  * Return JSON of all activities in /db/activities
  */
-const allPages = async (
-  req: NowRequest,
-  res: NowResponse
-): Promise<void> => {
+const allPages = async (req: NowRequest, res: NowResponse): Promise<void> => {
   const allPages = await getAllActivities();
   res.status(200).json(allPages);
 };
